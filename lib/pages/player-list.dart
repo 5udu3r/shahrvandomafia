f6a0c9141854263de5604ce0d50339ff19ea9b75
tokyodevs <darzi@protonmail.com>
2021-07-19 14:58:48 +0430

    fix pages scaffold

--- lib/pages/player-list.dart
+++ lib/pages/player-list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flui/flui.dart';
 
 class PlayerListScreen extends StatefulWidget {
   @override
@@ -9,24 +10,59 @@ class _PlayerListScreenState extends State<PlayerListScreen> {
   String _err='';
   @override
   Widget build(BuildContext context) {
-    return Theme(
-      data: ThemeData(hintColor: Color(0xff3D3D45)),
-      child: Scaffold(
-        backgroundColor: Theme.of(context).backgroundColor,
-        body: Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            Text(
-              "AI Powered Messaging App",
-              style: TextStyle(
-                color: Colors.white.withOpacity(0.8),
-                fontSize: 15,
-                fontWeight: FontWeight.w300,
-              ),
-            ),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('main screen'),
       ),
+      body: Column(
+        children: <Widget>[
+          FLFlatButton(
+            expanded: true,
+            color: Color(0xFF0F4C81),
+            textColor: Colors.white,
+            child: Text('شروع بازی', textAlign: TextAlign.center, style: TextStyle(fontFamily: 'IRANSans', fontSize: 24)),
+            onPressed: () =>  Navigator.pushNamed(context, '/choose_players'),
+          ),
+          FLFlatButton(
+            expanded: true,
+            color: Color(0xFF0F4C81),
+            textColor: Colors.white,
+            child: Text(' بازیکن ها', textAlign: TextAlign.center, style: TextStyle(fontFamily: 'IRANSans', fontSize: 24)),
+            onPressed: () => Navigator.pushNamed(context, '/players'),
+          ),
+          FLFlatButton(
+            expanded: true,
+            color: Color(0xFF0F4C81),
+            textColor: Colors.white,
+            child: Text(' نقش ها ', textAlign: TextAlign.center, style: TextStyle(fontFamily: 'IRANSans', fontSize: 24)),
+            onPressed: () =>  Navigator.pushNamed(context, '/roles'),
+          ),
+          FLFlatButton(
+            expanded: true,
+            color: Color(0xFF0F4C81),
+            textColor: Colors.white,
+            child: Text('کارت های حرکت آخر', textAlign: TextAlign.center, style: TextStyle(fontFamily: 'IRANSans', fontSize: 24)),
+            onPressed: () =>  Navigator.pushNamed(context, '/last_move_card'),
+          ),
+          FLFlatButton(
+            expanded: true,
+            color: Color(0xFF0F4C81),
+            textColor: Colors.white,
+            child: Text(' قرعه مرگ ', textAlign: TextAlign.center, style: TextStyle(fontFamily: 'IRANSans', fontSize: 24)),
+            onPressed: () =>  Navigator.pushNamed(context, '/last_chance'),
+          ),
+          FLFlatButton(
+            expanded: true,
+            color: Color(0xFF0F4C81),
+            textColor: Colors.white,
+            child: Text(' شب ', textAlign: TextAlign.center, style: TextStyle(fontFamily: 'IRANSans', fontSize: 24)),
+            onPressed: () =>  Navigator.pushNamed(context, '/night'),
+          ),
+
+          //all the children widgets that you need
+        ],
+      ),
+
     );
   }
 }
